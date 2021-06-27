775c28b118dce52cc73871c5e75beebc8813530a
bryannhughes <bryanhughes@hughesbuildingcompany.net>
2021-06-26 20:29:17 -0400

    Todo Component created

--- vue_board_game/src/App.vue
+++ vue_board_game/src/App.vue
@@ -1,15 +1,15 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template lang="html">
+  <div id="app">
+    <h1>To-Do List</h1>
+    
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
   }
 }
 </script>
